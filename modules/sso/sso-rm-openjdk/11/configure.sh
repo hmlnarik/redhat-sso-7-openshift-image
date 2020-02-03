9ee9d30224f6c85bb0b7e8348af5810682a20269
Hynek Mlnařík <hmlnarik@redhat.com>
2020-02-03 14:59:55 -0500

    fixup! KEYCLOAK-12861 Use JDK 11

--- modules/sso/sso-rm-openjdk/11/configure.sh
+++ modules/sso/sso-rm-openjdk/11/configure.sh
@@ -2,14 +2,21 @@
 set -u
 set -e
 
-if ! rpm -q java-11-openj9-devel; then
-  exit
+## Work around https://bugzilla.redhat.com/show_bug.cgi?id=1797054
+if rpm -q java-1.8.0-openjdk-headless && ( rpm -q java-11-openjdk-devel || rpm -q java-11-openj9-devel ); then
+    for pkg in java-1.8.0-openjdk-devel \
+           java-1.8.0-openjdk-headless \
+           java-1.8.0-openjdk; do
+        rpm -q "$pkg" && rpm -e --nodeps "$pkg"
+    done
 fi
 
-for pkg in java-11-openjdk-devel \
-       java-11-openjdk-headless \
-       java-11-openjdk; do
-    if rpm -q "$pkg"; then
-        rpm -e --nodeps "$pkg"
-    fi
-done
+## Work around OpenJDK being installed as dependency. https://bugzilla.redhat.com/show_bug.cgi?id=1762827 and similar
+if rpm -q java-11-openj9-devel; then
+    for pkg in java-11-openjdk-devel \
+           java-11-openjdk-headless \
+           java-11-openjdk; do
+        rpm -q "$pkg" && rpm -e --nodeps "$pkg"
+    done
+fi
+
